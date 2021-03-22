bc8dfd31db9c3cc5ad97c81672c784199f6f26ce
Natchanon Itthipalkul <43616464+drazter@users.noreply.github.com>
2021-03-22 12:35:48 +0700

    Update Temp_Upload.py

--- Temp_Upload.py
+++ Temp_Upload.py
@@ -1,11 +1,11 @@
 import urllib
 import urllib.request
 
-
-def upload(temp):  # Function to upload temperature(array) to thingspeak
-    key = '8OSIDCHTY7CEKVFL'
-    baseURL = 'https://api.thingspeak.com/update?api_key=%s' % key
-    field = ''
+# Function to upload temperature(array) to thingspeak
+def upload(temp):
+    key = '8OSIDCHTY7CEKVFL' # API Key for Thing Speak
+    baseURL = 'https://api.thingspeak.com/update?api_key=%s' % key #Base URL for Thing Speak API
+    field = '' #Create empty string for field
     for i in range(temp.size):
         field += '&field%s=%s' % (i + 1, temp[i])
     urllib.request.urlopen(baseURL + field)
